2f07c1a94e2478d4bde9105187bac526a9f325ce
NkmJunior <junior.noukam@insa-lyon.fr>
2024-05-23 12:07:48 +0200

    Update User.cpp

---
+++ class/User.cpp
@@ -0,0 +1,39 @@
+using namespace std;
+
+
+#include <iostream>
+#include <string>
+
+#include "User.h"
+
+
+Role User::get_role () const
+{
+    return role;
+}
+
+
+User::User ( const User & unUser )
+{
+    #ifdef MAP
+        cout << "Appel au constructeur de copie de <User>" << endl;
+    #endif
+        role=unUser.role;
+}
+
+
+User::User ( const Role lerole)
+{
+    #ifdef MAP
+        cout << "Appel au constructeur de <User>" << endl;
+    #endif
+  role=lerole;
+}
+
+
+User::~User ( )
+{
+    #ifdef MAP
+        cout << "Appel au destructeur de <User>" << endl;
+    #endif 
+}
