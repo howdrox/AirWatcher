4d66d6e3a0beff474283c0815fc998759f934f63
howdrox <louiskusno@gmail.com>
2024-05-23 21:51:57 +0200

    formating

--- src/Time.h
+++ src/Time.h
@@ -1,18 +1,13 @@
 #if !defined(Time_H)
 #define Time_H
 
-//--------------------------------------------------- Interfaces utilisées
 #include <iostream>
 #include <string>
 using namespace std;
 
 class Time
 {
-    //----------------------------------------------------------------- PUBLIC
-    //----------------------------------------------------- Méthodes publiques
-
 public:
-    //----------------------------------------------------- Méthodes publiques
     int getYear() const;
 
     int getMonth() const;
@@ -23,10 +18,8 @@ public:
 
     void setTime(const string &t);
 
-    //------------------------------------------------- Surcharge d'opérateurs
     bool operator<(const Time &date) const;
 
-    //-------------------------------------------- Constructeurs - destructeur
     Time(const Time &date);
 
     Time(int yy, int MM, int dd);
@@ -36,7 +29,6 @@ public:
     virtual ~Time();
 
 protected:
-    //----------------------------------------------------- Attributs protégés
     int year, month, day;
     double _time;
 };
