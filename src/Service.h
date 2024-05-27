d05d5fd8d0ffdabc79097d583e1a33f6c2a023c2
WyzzR <louiskusno@gmail.com>
2024-05-27 16:49:45 +0200

    changed sortSensors signature

--- src/Service.h
+++ src/Service.h
@@ -20,17 +20,17 @@ class Service
 public:
     double calculateQuality(const Zone &zone, const Time &start, const Time &end);
 
-    map<int, Measurement> filterMeasurements(const Time &start, const Time &end, map<int, vector<Measurement>> measurements); // a voir si `measurements` est en const
+    map<int, vector<Measurement>> filterMeasurements(const Time &start, const Time &end, map<int, vector<Measurement>> measurements); // a voir si `measurements` est en const
 
     double calculateImpactRadius(const int &cleanerId);
 
     double distance(const Coord &coord1, const Coord &coord2);
 
-    vector<Zone> getSimilarZones(const int &sensorID, const Time &start, const Time &end, const double &delta);
+    multimap<double, int> getSimilarZones(const int &sensorID, const Time &start, const Time &end, const double &delta);
 
     double calculateQuality(const map<int, Measurement> &measurements);
 
-    map<double, Sensor> sortSensors(vector<Sensor> sensors, const Coord &coord); // a voir si `sensors` peut être en const
+    map<double, Sensor> sortSensors(map<int, Sensor> sensors, const Coord &coord); // a voir si `sensors` peut être en const
 
     Service();
 
